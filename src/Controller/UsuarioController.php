6cdf3c789276693de4f343b2a9129b1b9a3c9c4a
lidloma <lidia.lopez.marchante.alu@iesfernandoaguilar.es>
2024-05-14 22:44:17 +0200

    TOKEN

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Usuario;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class UsuarioController extends AbstractController
@@ -16,4 +22,27 @@ class UsuarioController extends AbstractController
             'path' => 'src/Controller/UsuarioController.php',
         ]);
     }
+
+    #[Route('/api/login', name: 'app_login', methods: ['POST'])]
+    public function login(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager, Request $request, JWTTokenManagerInterface $JWTManager): JsonResponse
+    {
+
+        $body = json_decode($request->getContent(), true);
+
+        $email = $body['email'];
+        $contrasenia = $body['contrasenia'];
+
+        $usuario = $entityManager->getRepository(Usuario::class)->findOneBy([
+            'email' => $email,
+        ]);
+
+        if ($usuario && $passwordHasher->isPasswordValid($usuario, $contrasenia))
+        {
+            $token = $JWTManager->create($usuario);
+            return new JsonResponse(['token' => $token], Response::HTTP_OK);
+        }
+
+        return new JsonResponse (['message' => 'Incorrect credentials'], Response::HTTP_BAD_REQUEST);
+    }
+
 }
